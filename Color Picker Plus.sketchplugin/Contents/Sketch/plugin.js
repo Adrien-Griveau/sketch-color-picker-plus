7a88f2a8735d5ce010d56c6cab8120ef0af64283
Andrey Shakhmin <andrey.shakhmin@gmail.com>
2015-10-30 15:56:02 +0800

    Compatibility with OSX 10.10 (close #5)

--- Color Picker Plus.sketchplugin/Contents/Sketch/plugin.js
+++ Color Picker Plus.sketchplugin/Contents/Sketch/plugin.js
@@ -1,11 +1,9 @@
 
 var onRun = function(context) {
     var FRAMEWORK_NAME = "ColorPickerPlus";
-
 	try {
 		HKColorPickerPlus.pickBorderColor();        
 	} catch(e) {
-        print(e);
 		var pluginBundle = NSBundle.bundleWithURL(context.plugin.url()),
         	mocha = Mocha.sharedRuntime();
         if(mocha.loadFrameworkWithName_inDirectory(FRAMEWORK_NAME, pluginBundle.resourceURL().path())) {
